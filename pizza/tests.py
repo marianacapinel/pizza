ba0cf41a97cbe554ec8e6f7487c49856fec30113
Ivan Kalita <kaduev13@gmail.com>
2018-08-02 22:14:13 +0200

    Added delete order test.

--- pizza/tests.py
+++ pizza/tests.py
@@ -7,13 +7,16 @@ from pizza.models import Pizza, Client, Order
 
 
 class OrderTests(APITestCase):
+    def setUp(self):
+        # some fixtures that we can use in every test
+        self.pizza = Pizza.objects.create(name='Salami', size='Small', price=2)
+        self.customer = Client.objects.create(name='Test', address='Berlin')
+
     def test_create_order(self):
-        pizza = Pizza.objects.create(name='Salami', size='Small', price=2)
-        client = Client.objects.create(name='Test', address='Berlin')
         data = {
             'date': '2018-07-25',
-            'client': client.id,
-            'pizzas': [pizza.id]
+            'client': self.customer.id,
+            'pizzas': [self.pizza.id]
         }
 
         response = self.client.post('/orders/', data)
@@ -23,6 +26,15 @@ class OrderTests(APITestCase):
         # data from the request
         order_id = response.json()['id']
         order = Order.objects.get(id=order_id)
-        self.assertEqual(order.client, client)
-        self.assertEqual(list(order.pizzas.all()), [pizza])
+        self.assertEqual(order.client, self.customer)
+        self.assertEqual(list(order.pizzas.all()), [self.pizza])
         self.assertEqual(order.date, date(2018, 7, 25))
+
+    def test_delete_order(self):
+        order = Order.objects.create(client=self.customer, date=date(2018, 7, 25))
+        order.pizzas.set([self.pizza])
+
+        response = self.client.delete('/orders/{}/'.format(order.id))
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertIsNone(Order.objects.filter(id=order.id).first())
