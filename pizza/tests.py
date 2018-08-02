4346e1c780e4f4de6416ef7b2d0bdf74024179ea
Ivan Kalita <kaduev13@gmail.com>
2018-08-02 21:57:06 +0200

    Improved the test a little bit.
    Each test should be isolated and deterministic. That's why I changed
    hardcoded client and pizzas IDs in request data to the values from the
    database. Also I added the check that order was created with desired
    data.

--- pizza/tests.py
+++ pizza/tests.py
@@ -1,15 +1,28 @@
+from datetime import date
+
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Pizza, Client
+
+from pizza.models import Pizza, Client, Order
 
 
 class OrderTests(APITestCase):
+    def test_create_order(self):
+        pizza = Pizza.objects.create(name='Salami', size='Small', price=2)
+        client = Client.objects.create(name='Test', address='Berlin')
+        data = {
+            'date': '2018-07-25',
+            'client': client.id,
+            'pizzas': [pizza.id]
+        }
 
-       def test_create_order(self):
-                url = 'http://127.0.0.1:8000/orders/'
-                Pizza.objects.create(name="Salami", size="Small", price=2)
-                Client.objects.create(name="Test", address="Berlin")
-                data = {"date": "2018-07-25", "client": 1, "pizzas": [1]}
-                response = self.client.post(url, data, format='json')
-                print(response.content)
-                self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post('/orders/', data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # verifying that order was saved in the database and contains
+        # data from the request
+        order_id = response.json()['id']
+        order = Order.objects.get(id=order_id)
+        self.assertEqual(order.client, client)
+        self.assertEqual(list(order.pizzas.all()), [pizza])
+        self.assertEqual(order.date, date(2018, 7, 25))
